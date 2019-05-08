155c4093e85e890d7879d2e3e430737a20e704cc
michaelanggriawan <michaelanggriawan941@gmail.com>
2019-05-08 16:50:29 +0700

    Updates

---
+++ precache-manifest.38658f3d65a40980cc9f298fbbfd80a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9de9d4993cfae82b9f8313b6f9198e0",
+    "url": "/training_app/index.html"
+  },
+  {
+    "revision": "cb0e3cd2f136c1c0152d",
+    "url": "/training_app/static/css/2.7cc4b860.chunk.css"
+  },
+  {
+    "revision": "6aa45de02e959e198a1c",
+    "url": "/training_app/static/css/main.419a0f42.chunk.css"
+  },
+  {
+    "revision": "cb0e3cd2f136c1c0152d",
+    "url": "/training_app/static/js/2.6d38fb8f.chunk.js"
+  },
+  {
+    "revision": "6aa45de02e959e198a1c",
+    "url": "/training_app/static/js/main.84f50c44.chunk.js"
+  },
+  {
+    "revision": "a03d4ec4a254825d8b03",
+    "url": "/training_app/static/js/runtime~main.49c453a4.js"
+  }
+]);
